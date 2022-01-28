496fb2d3d7f9a7c4bc8765c95a242a5eea7546ee
Adu Lawrence <aduaondohemba@gmail.com>
2022-01-28 03:51:38 +0100

    Blog list component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const name = "Lawrence";
@@ -9,19 +10,24 @@ const Home = () => {
     { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+
+    setBlogs(newBlogs);
+  };
+
+  useEffect(() =>{
+    console.log('this is good');
+  }, []);
+
   return (
     <div className="home">
       <h1>Welcome {name}</h1>
-      {
-          blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by { blog.author}</p>
-            </div>
-          ))
-      }
+      <BlogList blogs = {blogs} handleDelete={ handleDelete }/>
+      
     </div>
   );
+  
 };
 
 export default Home;
